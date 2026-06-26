8e830cfbf9aebe193f7c02fb83ac5b9d1b53fe7d
iroha <noakst.1608@gmail.com>
2026-06-27 03:01:16 +0900

    fix: deadlock
    lockするmutexの順番と処理を変更。死亡通知と通常のプリントが競合しないように修正。

--- srcs/utils/print.c
+++ srcs/utils/print.c
@@ -6,7 +6,7 @@
 /*   By: nsato <nsato@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/23 21:46:42 by nsato             #+#    #+#             */
-/*   Updated: 2026/06/25 14:51:07 by nsato            ###   ########.fr       */
+/*   Updated: 2026/06/27 02:52:27 by nsato            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,24 +32,22 @@ bool	coder_print_status(t_coder *self, const char *status)
 {
 	long long	now_micro;
 	long long	now_milli;
-	bool		is_running;
 
-	pthread_mutex_lock(&self->data->print_mutex);
 	pthread_mutex_lock(&self->data->time_mutex);
-	is_running = self->data->is_simulation_running;
-	pthread_mutex_unlock(&self->data->time_mutex);
-	if (is_running)
+	if (!self->data->is_simulation_running)
 	{
-		now_micro = get_time() - self->data->simulation_start_time;
-		now_milli = now_micro / 1000LL;
-		if (printf("%lld %d %s\n", now_milli, self->id, status) < 0)
-		{
-			pthread_mutex_lock(&self->data->time_mutex);
-			self->data->is_simulation_running = false;
-			pthread_cond_broadcast(&self->data->exit_cond);
-			pthread_mutex_unlock(&self->data->time_mutex);
-		}
+		pthread_mutex_unlock(&self->data->time_mutex);
+		return (false);
+	}
+	pthread_mutex_lock(&self->data->print_mutex);
+	now_micro = get_time() - self->data->simulation_start_time;
+	now_milli = now_micro / 1000LL;
+	if (printf("%lld %d %s\n", now_milli, self->id, status) < 0)
+	{
+		self->data->is_simulation_running = false;
+		pthread_cond_broadcast(&self->data->exit_cond);
 	}
 	pthread_mutex_unlock(&self->data->print_mutex);
-	return (is_running);
+	pthread_mutex_unlock(&self->data->time_mutex);
+	return (true);
 }
